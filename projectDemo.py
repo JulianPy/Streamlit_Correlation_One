749538eb7c8900f91ceabe41138a0d4159b1978b
JulianPy <jdvargasl@unal.edu.co>
2022-06-29 20:56:48 -0500

    Hola

---
+++ projectDemo.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+def cabecera():
+    st.title("Colombia Compra Eficiente")
+
+def imagen():
+    image = Image.open('colombiaCompra.png')
+    st.image(image, caption='Sunrise by the mountains')
+def botoncito():
+    # Cargar el Archivo
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # Lectura del Archivo
+        #st.write("filename:", uploaded_file.name)
+        st.subheader('Validation Table')
+        df1 = pd.read_excel(uploaded_file)
+        st.dataframe(df1)
+        st.markdown('A validation table is shown with the complete results of the classification.'
+                    ' It gives you a suggestion to change your **UNSPC** code or a recommendation'
+                    ' for your contract ')
+        st.subheader('Words Count')
+        image = Image.open('servicios.png')
+        st.image(image, caption='Sunrise by the mountains')
+        # Grafica
+        #g = df1['Sale Price']
+        #st.line_chart(g)
+
+
+            #bytes_data = uploaded_file.read()
+            #st.write("filename:", uploaded_file.name)
+            #st.write(bytes_data)
+            #st.write('Buena may')
+            #data = {'Contract number':[123456, 24561012, 369121518, 48121620, 510152025],
+            #        'Contract Type': ['Food', 'Integral Services', 'Construction materials', 'Health', 'Others'],
+            #        'Probability': [0.99, 0.97, 0.94, 0.90, 0.85]}
+            #df = pd.DataFrame(data)
+
+imagen()
+cabecera()
+botoncito()
