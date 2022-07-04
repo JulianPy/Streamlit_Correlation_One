69c6accbfaef5fafb3ac5385df927e4d20eb3a9f
Laura Valeria Navarro Avellaneda <lauravalerianavarroavellaneda@MacBook-Air-de-Laura.local>
2022-07-03 19:57:56 -0500

    Estilos página resultados

--- pages/2_📉_Results.py
+++ pages/2_📉_Results.py
@@ -16,8 +16,9 @@ st.sidebar.markdown("# Results")
 
 st.markdown('Here you will find the code that best suits your contract. The suggestion is given until class, to complete the last 2 digits of your code, we recommend you access the Colombia Compra Eficiente page where you can find the codes for products: [Classifier of goods and services](https://www.colombiacompra.gov.co/clasificador-de-bienes-y-Servicios) ')
 col1f,col2f = st.columns(2)
-col1f.subheader('Best code')
-col2f.metric('', '101015','98%')
+col1f.subheader('Best code suggestion:')
+col2f.text('101015'+'   '+ '98%')
+
 
 col1,col2,col3 = st.columns(3)
 col1.metric('Segment', '10', '70%')
--- 🏠_Home.py
+++ 🏠_Home.py
@@ -10,17 +10,9 @@ st.set_page_config(
 
 st.sidebar.markdown("# Home")
 
-header = st.container()
-dataset = st.container()
-features = st.container()
-modelTraining = st.container()
+image = Image.open('home.png')
+st.image(image, caption='')
+image2 = Image.open('home2.png')
+st.image(image2, caption='')
+st.markdown('[* Source: Colombia Compra Eficiente](https://www.colombiacompra.gov.co/sites/cce_public/files/cce_clasificador/manualclasificador.pdf)')
 
-
-def imagen():
-    image = Image.open('home.png')
-    st.image(image, caption='')
-    image2 = Image.open('home2.png')
-    st.image(image2, caption='')
-    st.markdown('[* Source: Colombia Compra Eficiente](https://www.colombiacompra.gov.co/sites/cce_public/files/cce_clasificador/manualclasificador.pdf)')
-
-imagen()
